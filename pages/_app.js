b0e50e453ed43260821a8222e00d802b45eda39b
elliehachemjob <elliehachemjob@gmail.com>
2021-11-15 10:55:22 +0200

    Nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //this page render all component along with pageprops
+  return (
+    <Layout>
+      {/* render custom things here if needed for each page*/}
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from "next/head"; //for seo
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>title</title>
+        <meta name="keywords" content="website,about, programming" />
+      </Head>
+      <h1>welcome to about page</h1>
+    </div>
+  );
+}
